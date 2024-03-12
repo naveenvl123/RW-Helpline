1039cd6485d2db164714688a0ee857ef93ac406a
Naveen <naveen.shanmugam@verticurl.com>
2024-03-12 16:45:33 +0530

    new update

--- component.js
+++ component.js
@@ -2,12 +2,25 @@
 class AppHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-      <a href="index.html"><span>Home</span></a>
-      <a href="naming.html"><span>Naming</span></a>
-      <a href="2.html"><span>OFT</span></a>
-      <a href="picklist.html"><span>Picklist</span></a>
-      <a href="prank.html" tar>Danger☢️</a>
+<a href="index.html"><span>Home</span></a>
+<a href="naming.html"><span>Naming</span></a>
+<a href="2.html"><span>OFT</span></a>
+<a href="picklist.html"><span>Picklist</span></a>
+<a href="customertype.html"><span>Customer Type</span></a>
+<a href="industry.html"><span>Industry</span></a>
+<a href=".html"><span></span></a>
+<a href=".html"><span></span></a>
+<a href="prank.html" tar>Danger☢️</a>
         `
     }
 }
 window.customElements.define('app-header', AppHeader)
+
+class copiedSuccess extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+<div id="copied-success" class="copied">Copied</div>
+        `
+    }
+}
+window.customElements.define('dan-copied', copiedSuccess)
