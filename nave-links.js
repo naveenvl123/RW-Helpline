e507e852838819cb1559a6493807889cb35d90c4
Naveen <naveen.shanmugam@verticurl.com>
2024-05-03 18:49:29 +0530

    new update

--- nave-links.js
+++ nave-links.js
@@ -7,10 +7,7 @@ class AppHeader extends HTMLElement {
 <a href="email-naming.html"><span>Naming</span></a>
 <a href="email.html"><span>Email</span></a>
 <a href="email-oft.html"><span>OFT</span></a>
-<a href="segments-customertype.html"><span>Customer Type</span></a>
-<a href="segments-industry.html"><span>Industry</span></a>
-<a href="segments-jobfunction.html"><span>Job Function</span></a>
-<a href="segments-joblevel.html"><span>Job Level</span></a>
+<a href="segments-picklist.html"><span>Picklist</span></a>
 <a href="prank.html" tar>Danger☢️</a>
  `
     }
